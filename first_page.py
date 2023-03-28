eb401eb91eb7da50b156b0a4e9a4f4a20d445400
thapelo <tmmakola021@student.wethinkcode.co.za>
2023-03-28 04:27:41 +0200

    Initial commit

---
+++ first_page.py
@@ -0,0 +1,13 @@
+import random
+def login():
+    userName = input("Enter your name:")
+    userSurname =("Enter you surname:")
+    initialize()
+
+def initialize():
+    theList =[]
+    prompt = input("Press Enter to begin")
+    if prompt == " ":
+        for i in range(1-59):
+            theList.append(random())
+        print(theList)
