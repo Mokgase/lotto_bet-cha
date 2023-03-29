ea70db1c760bd69d93e25d3eb3e9869fdd41b7ec
thapelo <tmmakola021@student.wethinkcode.co.za>
2023-03-29 13:12:01 +0200

    organised functions

--- first_page.py
+++ first_page.py
@@ -1,26 +1,42 @@
 import random
-import numpy
+
 def login():
+    global userName
+    global userSurname
     userName = input("Enter your name:")
-    userSurname =("Enter you surname:")
+    userSurname = input("Enter you surname:")
     initialize()
 
 def initialize():
-    theList =[]
     userInput = input("Press Enter to begin")
+    if userInput == "":
+        looper()
+    else:
+        initialize()
+
+def looper():
+    theList =[]
     while True:
-        if userInput == "":
-            for i in range(6):
-                theList.append(random.randint(1-59))
-                finalList = numpy.array(theList)
-                content=str(finalList)
-                file.write(content + "\n")
-                file.close()
-            print ("here are the lucky lotto picks")
-            print(theList)
-        elif userInput != "":
-            print("Press enter if you want to begin")
-        else:
-            exit()
+        for i in range(6):
+            theList.append(random.randint(1-59))
+            file=open("lottoList.txt", "a" + "\n")
+            file.write("theList")
+            file.close()
+        print ("here are the lucky lotto picks")
+        print(theList)
+        continuePrompt()
+    
+
+def continuePrompt(userName,userSurname):
+    listOfAnswers1=["Y","YES","Yes" ]
+    listOfAnswers2=["N","NO","No","no","n"]
+    userInput =input("Do you still want more lotto numbers?")
+    if userInput == listOfAnswers1:
+        looper()
+    elif userInput == listOfAnswers2:
+        print("Alright then "+ userName + "" + userSurname + ".")
+        exit()
+        
 
 
+login()
