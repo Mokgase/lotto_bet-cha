d6c4ebdf207e2316592c83449a40544a08f24506
thapelo <tmmakola021@student.wethinkcode.co.za>
2023-03-30 17:57:54 +0200

    redux

--- first_page.py
+++ first_page.py
@@ -8,7 +8,7 @@ def login():
     initialize()
 
 def initialize():
-    global userInput
+    # global userInput
     userInput = input("Press B to begin: ")
     if userInput == "b" or "B":
         looper()
@@ -23,23 +23,38 @@ def initialize():
     #     file.close()
 
 def looper():
-    theList =[]
-    while True:
-        #generates 6 numbers and puts them in a list
-        for i in range(6):
-            theList.append(random.randint(1,59))
-            print(theList)
-            print ("here are the lucky lotto picks")
-            return theList
+     theList = []
+     i = 1
+     while i < 7:
+          theList.append(random.randint(1,59))
+          if i == 7:
+            break
+          else:
+            i+=1
+            for x in theList:
+                finalList = x
+                file = open("lottoList.txt","a")
+                file.write(finalList + "\n")
+                file.close()
+                print("Here are your lucky lotto picks:" + "\n" + finalList)
+            continuePrompt()
+                
         
-        #itterates through the list and converts it to a string and writes it into a file
-        for x in theList:
-            finalPick = print(x)
-            appender = "lottoList.txt"
-            file = open(appender, "a")
-            file.write(finalPick + "\n")
-            file.close()
-            continuePrompt(userName,userSurname)
+    # theList =[]
+    # while True:
+    #     #generates 6 numbers and puts them in a list
+    #     for i in range(6):
+    #         theList.append(random.randint(1,59))
+        
+    #     #itterates through the list and converts it to a string and writes it into a file
+    #     for x in theList:
+    #         finalPick = x
+    #         appender = "lottoList.txt"
+    #         file = open(appender, "a")
+    #         file.write(finalPick + "\n")
+    #         file.close()
+    #         print ("here are the lucky lotto picks" + "\n" + finalPick)
+    #     continuePrompt()
 
 
         # for i in range(6):
@@ -55,14 +70,15 @@ def looper():
         #     continuePrompt(userName,userSurname)
     
 
-def continuePrompt(userName,userSurname):
-    listOfAnswers1=["Y","YES","Yes" ]
-    listOfAnswers2=["N","NO","No","no","n"]
-    userInput =input("Do you still want more lotto numbers?")
-    if userInput == listOfAnswers1[0][1][2]:
-        looper()
-    elif userInput == listOfAnswers2[0][1][2][3][4]:
-        print("Alright then "+ userName + "" + userSurname + ".")
+def continuePrompt():
+        print("we here " + userName + " " + userSurname)
+    # listOfAnswers1=["Y","YES","Yes","yes", "y"]
+    # listOfAnswers2=["N","NO","No","no","n"]
+    # userInput =input("Do you still want more lotto numbers?")
+    # if userInput == listOfAnswers1[0] or [1] or[2] or [3] or [4]:
+    #     looper()
+    # elif userInput == listOfAnswers2[0] or [1] or [2] or[3] or [4]:
+    #     print("Alright then "+ userName + "" + userSurname + ".")
         exit()
         
 
