cb2b4b6984e61098a85686d8d60f7f751e5a1138
thapelo <tmmakola021@student.wethinkcode.co.za>
2023-04-02 03:58:20 +0200

    refactored functions

--- first_page.py
+++ first_page.py
@@ -1,19 +1,21 @@
 import random
+import string
 
 def login():
     global userName
     global userSurname
     userName = input("Enter your name:")
     userSurname = input("Enter you surname:")
-    initialize()
+    
 
 def initialize():
-    # global userInput
-    userInput = input("Press B to begin: ")
-    if userInput == "b" or "B":
-        looper()
-    else:
-        initialize()
+    global userInput
+    while True :
+        userInput = input("Press Enter to begin: ")
+        if userInput == "":
+            looper()
+        else:
+            initialize()
 
 
     #  appender = "animeGenre.txt"
@@ -23,21 +25,37 @@ def initialize():
     #     file.close()
 
 def looper():
-     theList = []
-     i = 1
-     while i < 7:
-          theList.append(random.randint(1,59))
-          if i == 7:
-            break
-          else:
-            i+=1
-            for x in theList:
-                finalList = x
-                file = open("lottoList.txt","a")
-                file.write(finalList + "\n")
-                file.close()
-                print("Here are your lucky lotto picks:" + "\n" + finalList)
-            continuePrompt()
+    theList = []
+    finalList = []
+    for i in range(6):
+        if i < range(6):
+            theList.append(random.randint(1,59))
+        else:
+            print("Done generating")
+        break
+    for x in theList:
+        finalList.append(str(x))
+        print("Here's your lucky lotto numbers "+finalList+" .")
+        theFile = "lottoList.txt"
+        file = open(theFile, "a")
+        file.write(finalList + "\n")
+        file.close()
+
+    #  theList = []
+    #  i = 1
+    #  while i < 7:
+    #       theList.append(random.randint(1,59))
+    #       if i == 7:
+    #         break
+    #       else:
+    #         i+=1
+    #         for item in theList:
+    #             finalList = print(str(item))
+    #             file = open("lottoList.txt","a")
+    #             file.write(finalList + "\n")
+    #             file.close()
+    #             print("Here are your lucky lotto picks:" + "\n" + finalList)
+    #         continuePrompt()
                 
         
     # theList =[]
@@ -71,16 +89,19 @@ def looper():
     
 
 def continuePrompt():
-        print("we here " + userName + " " + userSurname)
-    # listOfAnswers1=["Y","YES","Yes","yes", "y"]
-    # listOfAnswers2=["N","NO","No","no","n"]
-    # userInput =input("Do you still want more lotto numbers?")
-    # if userInput == listOfAnswers1[0] or [1] or[2] or [3] or [4]:
-    #     looper()
-    # elif userInput == listOfAnswers2[0] or [1] or [2] or[3] or [4]:
-    #     print("Alright then "+ userName + "" + userSurname + ".")
+    print("we here " + userName + " " + userSurname)
+    listOfAnswers1=["Y","YES","Yes","yes", "y"]
+    listOfAnswers2=["N","NO","No","no","n"]
+    userInput =input("Do you still want more lotto numbers?")
+    if userInput == listOfAnswers1[0] or [1] or[2] or [3] or [4]:
+        looper()
+    elif userInput == listOfAnswers2[0] or [1] or [2] or[3] or [4]:
+        print("Alright then "+ userName + "" + userSurname + ".")
         exit()
         
 
 
 login()
+initialize()
+looper()
+continuePrompt()
